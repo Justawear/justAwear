0e30a279e53cd3bed6b6cdf617be316d3db0bd23
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2022-01-13 15:15:40 -0600

    schemas/index.js file updated

--- server/config/connections.js
+++ server/config/connections.js
@@ -1,5 +1,7 @@
+// importing mongoose
 const mongoose = require('mongoose');
 
+// telling Mongoose which database we want to connect to
 mongoose.connect(process.env.MONGODB_URI || 
     'mongodb://localhost/justawear', {
         useNewUrlParser: true, 
