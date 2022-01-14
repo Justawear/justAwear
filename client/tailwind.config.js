dfd23f7954ad6885e668bfafa20946c1c5b083cc
Lance Kelley <lancekkcodes@gmail.com>
2022-01-13 20:21:22 -0600

    initial tailwind setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  mode: 'jit',
+  theme: {
+    extend: {},
+    screens: {
+      xl: {max: "1279px"},
+      lg: {max: "1023px"},
+      md: {max: "767px"},
+      sm: {max: "639px"}
+    }
+  },
+  content: [],
+  variants: {},
+  plugins: [
+  ],
+}
