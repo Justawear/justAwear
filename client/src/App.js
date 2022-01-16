7e1de2a12ef426f9642877f7ba1bb6a60a1125b0
Adam Romano <adamromano@Adams-iMac.lan>
2022-01-16 09:54:13 -0600

    adding order history page

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,10 @@ import Home from './pages/Home';
 // import Detail from './pages/Detail';
 // import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
-// import Signup from './pages/Signup';
+import Signup from './pages/Signup';
 import Nav from './components/Nav';
-// import Success from './pages/Success';
-// import OrderHistory from './pages/OrderHistory';
+import Success from './pages/Success';
+import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -47,10 +47,10 @@ export default function App() {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
-              {/*<Route exact path="/signup" component={Signup} />
+              <Route exact path="/signup" component={Signup} />
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
+              {/* <Route exact path="/products/:id" component={Detail} />
   <Route component={NoMatch} /> */}
             </Switch>
         </div>
